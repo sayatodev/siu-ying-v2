8b5445b0551093bc0af14e5eb04d7996454eee37
Sayako Hazuki <amf.kano@gmail.com>
2025-04-08 18:19:10 +0800

    Update disclaimer

--- src/commands/dselife.ts
+++ src/commands/dselife.ts
@@ -74,7 +74,7 @@ export default {
 				return `[${pp}](https://dse.life/static/pp/${subj}/dse/${year}/${pp}.pdf)`;
 			}).join('  ');
 			await interaction.editReply({
-				content: `## ${year} ${subj}\nSearch Results:\n${body}\n-# This command is only a search tool. dse.life may shut down their services anytime.`,
+				content: `## ${year} ${subj}\nSearch Results:\n${body}\n-# This command is only a search tool. We do not host any files.`,
 			});
 		} catch (error) {
 			console.error(error);
